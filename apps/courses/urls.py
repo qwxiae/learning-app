03cd9cce4c3f69c0f21c3a75a1455a35e7c34d5a
qwx <qwxiae@gmail.com>
2026-03-02 19:13:37 +0300

    cerate urls.py for courses

--- apps/courses/urls.py
+++ apps/courses/urls.py
@@ -1,4 +1,3 @@
-# apps/core/urls.py
 from django.urls import path
 from . import views
 
@@ -7,4 +6,22 @@ app_name = "courses"
 urlpatterns = [
     path("", views.home_view, name="home"),
     path("about/", views.about_view, name="about"),
+    
+    # === Catalog ===
+    # search and list all courses
+    path("catalog/", views.catalog_view, name="catalog"),
+    # list all categories
+    path("catalog/categories/", views.categories_view, name="categories"),
+    path("catalog/categories/<slug:slug>/", views.category_view, name="category_courses"),
+
+
+    # === Courses ====
+    path("courses/<slug:slug>/", views.course_detail_view, name="course_detail"),
+    
+    path("courses/<slug:slug>/enroll/", views.enroll_view, name="enroll"),
+    path("courses/<slug:slug>/unenroll/", views.unenroll_view, name="unenroll"),
+
+    # show authenticated users enrolled courses
+    path("learn/", views.my_courses_view, name="my_courses"),
+
 ]

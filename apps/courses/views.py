9e6eecfb61c35f67651f651717413c6a1dcc2305
qwx <qwxiae@gmail.com>
2026-03-02 21:30:15 +0300

    add temporary templates for views

--- apps/courses/views.py
+++ apps/courses/views.py
@@ -5,3 +5,28 @@ def home_view(request):
 
 def about_view(request):
     return render(request, "courses/about.html")
+
+def catalog_view(request):
+    return render(request, "courses/home.html")
+
+def categories_view(request):
+    return render(request, "courses/home.html")
+
+def category_view(request, category_id: int):
+    return render(request, "courses/home.html")
+
+def course_detail_view(request, course_id: int):
+    return render(request, "courses/home.html")
+
+# Only for authenticated users
+def enroll_view(request, course_id: int):
+    return render(request, "courses/home.html")
+
+# Only for authenticated users
+def unenroll_view(request, course_id: int):
+    return render(request, "courses/home.html")
+
+# Only for authenticated users
+def my_courses_view(request):
+    return render(request, "courses/home.html")
+

7bf9693e75ce43ec809c3c150923c530765b2a45
qwx <qwxiae@gmail.com>
2026-03-06 01:39:48 +0300

    optimize queries; sovle n+1 problem

--- apps/courses/views.py
+++ apps/courses/views.py
@@ -1,32 +1,111 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Category, Course, Enrollment
+from django.views.decorators.http import require_POST
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import get_user_model
+from django.db.models import Count, Prefetch
+
+User = get_user_model()
 
 def home_view(request):
-    return render(request, "courses/home.html")
+    featured_courses = Course.objects.filter(
+        is_published=True
+    ).select_related("category", "author").order_by('-created_at')[:6]
+
+    return render(request, "courses/home.html", {
+        "featured_courses": featured_courses,
+        "categories": Category.objects.all(),
+    })
 
 def about_view(request):
+    """ Information about the site"""
     return render(request, "courses/about.html")
 
 def catalog_view(request):
-    return render(request, "courses/home.html")
+    categories = Category.objects.all()
+    courses = Course.objects.select_related("category", "author").filter(is_published=True)
 
-def categories_view(request):
-    return render(request, "courses/home.html")
+    category_slug = request.GET.get("category")
+    # search if provided
+    q = request.GET.get("q")
 
-def category_view(request, category_id: int):
-    return render(request, "courses/home.html")
+    if category_slug:                                         
+        courses = courses.filter(category__slug=category_slug)
+    if q:
+        courses = courses.filter(title__icontains=q)
 
-def course_detail_view(request, course_id: int):
-    return render(request, "courses/home.html")
+    return render(request, "courses/catalog.html", {
+        "courses": courses,
+        "categories": categories,
+        "current_category": category_slug,
+        "q": q
+    })
 
-# Only for authenticated users
-def enroll_view(request, course_id: int):
-    return render(request, "courses/home.html")
+def course_detail_view(request, slug: str):
+    course = get_object_or_404(
+        Course.objects
+        .select_related("author", "category")
+        .annotate(module_count=Count("modules")), 
+        slug=slug,
+        is_published=True
+    )
+    
+    # modules = (
+    #     course.modules
+    #     .prefetch_related("lessons")
+    #     .annotate(lesson_count=Count("lessons"))
+    #     .order_by("order")
+    # )
 
-# Only for authenticated users
-def unenroll_view(request, course_id: int):
-    return render(request, "courses/home.html")
+    is_enrolled = False
+    if request.user.is_authenticated:
+        is_enrolled = Enrollment.objects.filter(
+            user=request.user,
+            course=course
+        ).exists()
 
-# Only for authenticated users
+    return render(request, "courses/course_detail.html", {
+        "course": course,
+        # "modules": modules,
+        "is_enrolled": is_enrolled
+    })
+
+@login_required
+@require_POST
+def enroll_view(request, slug: str):
+    course = get_object_or_404(Course, slug=slug, is_published=True)
+    Enrollment.objects.get_or_create(user=request.user, course=course)
+
+    if request.headers.get("HX-Request"):
+        return render(request, "partials/enroll_button.html", {
+            "course": course,
+            "is_enrolled": True
+        })
+    
+    return redirect("courses:course_detail", slug=slug)
+
+@login_required
+@require_POST
+def unenroll_view(request, slug: str):
+    course = get_object_or_404(Course, slug=slug, is_published=True)
+    Enrollment.objects.filter(user=request.user, course=course).delete()
+
+    if request.headers.get("HX-Request"):
+        return render(request, "partials/enroll_button.html", {
+            "course": course,
+            "is_enrolled": False
+        })
+
+    next_url = request.POST.get("next") or request.META.get("HTTP_REFERER") or "courses:my_courses"
+    return redirect(next_url)
+
+@login_required
 def my_courses_view(request):
-    return render(request, "courses/home.html")
+    enrollments = Enrollment.objects.filter(
+        user=request.user
+    ).select_related("course", "course__category", "course__author")
+
+    return render(request, "courses/my_courses.html", {
+        "enrollments": enrollments
+    })
 

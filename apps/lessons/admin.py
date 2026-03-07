f279bf4bf7266e85cc638c0aeba189d0595efcf1
qwx <qwxiae@gmail.com>
2026-03-07 18:28:45 +0300

    create admin for modules in lessons app

--- apps/lessons/admin.py
+++ apps/lessons/admin.py
@@ -1,3 +1,69 @@
 from django.contrib import admin
+from . models import (
+    Lesson, Step,
+    TheoryStep, TextInputStep, 
+    ChoiceStep, ChoiceOption,
+    ProgrammingStep, TestCase,
+) 
 
-# Register your models here.
+
+class StepInline(admin.TabularInline):
+    model = Step
+    extra = 1
+    fields = ["title", "type", "order"]
+
+class ChoiceOptionInline(admin.TabularInline):
+    model = ChoiceOption
+    extra = 2
+    fields = ["text", "is_correct", "order"]
+
+class TestCaseInline(admin.TabularInline):
+    model = TestCase
+    extra = 1
+    fields = ["input_data", "expected_output", "order"]
+
+@admin.register(Step)
+class StepAdmin(admin.ModelAdmin):
+    list_display = ["title", "lesson", "type", "order"]
+    list_filter = ["type"]
+    search_fields = ["title", "lesson__title"]
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ["public_id", "title", "module", "order", "is_published"]
+    list_filter = ["is_published", "module__course"]
+    search_fields = ["title", "module__title"]
+    readonly_fields = ["public_id"]
+    inlines = [StepInline]
+
+@admin.register(TheoryStep)
+class TheoryStepAdmin(admin.ModelAdmin):
+    list_display = ["title", "lesson", "order"]
+    search_fields = ["title"]
+    fields = ["lesson", "title", "order", "html_content"]
+
+@admin.register(ChoiceStep)
+class ChoiceStepAdmin(admin.ModelAdmin):
+    list_display = ["title", "lesson", "order", "is_multiple"]
+    search_fields = ["title"]
+    fields = ["title", "lesson", "order", "question_text", "is_multiple"]
+    inlines = [ChoiceOptionInline]
+
+@admin.register(ProgrammingStep)
+class ProgrammingStepAdmin(admin.ModelAdmin):
+    list_display = ["title", "lesson", "order", "language"]
+    search_fields = ["title"]
+    list_filter = ["language"]
+    fields = [
+        "lesson", "title", "order",
+        "question_text", "language",
+        "time_limit_ms", "memory_limit_mb",
+        "solution_template"
+    ]
+    inlines = [TestCaseInline]
+
+@admin.register(TextInputStep)
+class TextInputStepAdmin(admin.ModelAdmin):
+    list_display = ["title", "lesson", "order"]
+    search_fields = ["title"]
+    fields = ["title", "lesson", "order", "question_text", "answer"]

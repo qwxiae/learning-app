e2ec1044da4276afdbe9cb046f18ca353e3e038d
qwx <qwxiae@gmail.com>
2026-03-09 18:54:23 +0300

    store seeds in one place for testing ease

---
+++ apps/management/commands/seed.py
@@ -0,0 +1,10 @@
+from django.core.management import BaseCommand, call_command
+
+class Command(BaseCommand):
+    help = "Test data for all apps in project"
+
+    def handle(self, *args, **kwargs):
+        call_command("seed_roles")
+        call_command("seed_categories")
+        call_command("seed_courses")
+        call_command("seed_lessons")
